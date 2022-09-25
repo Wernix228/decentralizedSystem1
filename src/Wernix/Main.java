4189f826576569c60a1b1fcafa6a267434bcb070
Wernix <Ktoto2667@gmail.com>
2022-09-25 20:38:25 +0300

    Add first words

---
+++ src/Wernix/Main.java
@@ -0,0 +1,15 @@
+package Wernix;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+    https://youtu.be/4gmEoQ8ucew
+
+    https://habr.com/ru/company/web3_tech/blog/678062/
+
+    https://javarush.ru/groups/posts/2546-java-i-blokcheyn-naydetsja-li-v-tekhnologii-budujshego-mesto-dlja-java-koda
+
+	 */
+    }
+}
